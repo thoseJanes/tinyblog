f1d5a97ded9e30b030ed571c581999de11696282
Eleanor <dlutzhouzijian@163.com>
2025-08-11 02:06:49 +0100

    refactor: reoganize directory

--- aiservice/config/config.py
+++
@@ -1,7 +0,0 @@
-import os
-import yaml
-
-config_file = open(os.path.join(os.path.dirname(__file__), "aiservice.yaml"), encoding="UTF-8")
-Config = yaml.load(config_file, yaml.FullLoader)
-
-config_file.close()
--- aiservice/internal/pkg/mysql/mysql.py
+++ aiservice/internal/pkg/mysql/mysql.py
@@ -1,6 +1,8 @@
 from dbutils.pooled_db import PooledDB
 import pymysql
 
+from configs.config import Config
+mysql_config = Config["mysql"]
 # 创建连接池
 MysqlPool = PooledDB(
     creator=pymysql,  # 使用的数据库模块
@@ -8,9 +10,9 @@ MysqlPool = PooledDB(
     mincached=3,  # 初始化时创建的闲置连接数
     maxcached=5,  # 池中最多闲置连接数
     blocking=True,  # 连接池满时是否阻塞等待
-    host='localhost',
-    user='aiservice',
-    password='123456',
-    database='tinyblog',
+    host=mysql_config['host'],
+    user=mysql_config['user'],
+    password=mysql_config['password'],
+    database=mysql_config['database'],
     charset='utf8mb4'
 )
